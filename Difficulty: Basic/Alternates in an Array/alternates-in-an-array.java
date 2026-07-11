918108ce5802d433e554bc5f791a9a9822e34697
Durga Neha <nehakollipara169@gmail.com>
2026-07-11 19:38:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Alternates in an Array/alternates-in-an-array.java
+++ Difficulty: Basic/Alternates in an Array/alternates-in-an-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public ArrayList<Integer> getAlternates(int arr[]) {
         // Code Here
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        for(int i = 0; i < arr.length; i+=2){
-            res.add(arr[i]);
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < arr.length; i += 2){
+            ans.add(arr[i]);
         }
-        return res;
+        return ans;
     }
 }
